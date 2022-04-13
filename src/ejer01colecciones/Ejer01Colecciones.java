3f3a8e61d6074b4222a14955802e578ea2770fbe
Emir Armentano Gaset <emirgaset860@gmail.com>
2022-04-12 22:02:47 -0300

    Segundo ejercicio de Coleccicones

--- src/ejer01colecciones/Ejer01Colecciones.java
+++ src/ejer01colecciones/Ejer01Colecciones.java
@@ -1,6 +1,8 @@
 package ejer01colecciones;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Ejer01Colecciones {
@@ -30,6 +32,35 @@ public class Ejer01Colecciones {
         //Muestre list
         
         System.out.println("Las mascotas de la lista son ");
+        for (String aux : listaMascotas) {
+            System.out.println(aux);
+        }
+        
+        System.out.println("-------------------------------------------------");
+        
+        //Eliminar mascota
+        
+        System.out.println("Que raza quiere eliminar??");
+        String razaEliminar = leer.next();
+        Iterator<String> iT = listaMascotas.iterator();
+        int cont = 0;
+        while (iT.hasNext()){
+            
+            if (iT.next().equals(razaEliminar)) {
+                iT.remove();
+                cont ++;
+            }
+            
+        //Si no se encuentra raza, se le informa y se muestra lista ordenanda.
+        
+        }
+        if (cont == 0) {
+            System.out.println("No se encontro la Raza"); 
+        }
+        
+        Collections.sort(listaMascotas);
+        
+        System.out.println("Se imprime lista ordenada");
         for (String aux : listaMascotas) {
             System.out.println(aux);
         }
